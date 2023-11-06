9b5125f8bf0f42f1ffd688c4e473304233a4f923
Maheswara13444 <141887237+Maheswara13444@users.noreply.github.com>
2023-11-06 13:51:27 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukkan Umur");
+        int umur = input.nextInt();
+        if (umur >= 17){
+        System.out.println("Boleh Dapet Sim");
+    
+
+    }else {
+        System.out.println("Bocil Gak Usah kakean tingkah");
+}
+    }
+}
+
+
+
